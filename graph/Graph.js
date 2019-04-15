da943cae66b78e69985cad60895be292cf1cc183
Nhu Nguyen <nhutnguyen5691@gmail.com>
2019-04-15 02:37:58 -0700

    added Graph.js

---
+++ graph/Graph.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function createGraphFramework(){
+    
+}
+
+const nodePrototype = {
+    draw: function(g2) {
+         
+    },
+    translate: function(dx, dy){
+         
+    },
+    contains: function(point){},
+    getConnectionPoint: function(point){},
+    getBounds: function(){},
+    clone: function(){}
+}
+
+const edgePrototype = {
+    draw: function(g2){},
+    contains: function(point){},
+    connect: function(start, end){},
+    getStart: function(){},
+    getEnd: function(){},
+    getConnectionPoints: function(){},
+    getBounds: function(g2){},
+    clone: function(){}
+}
