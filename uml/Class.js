34de8804eda956123a43e0c6bc6cb1fe3dfecef5
Jeffrey Wu <Jeffrey.S.Wu@sjsu.edu>
2019-04-20 15:44:39 -0700

    updated nodes in class.js

--- uml/Class.js
+++ uml/Class.js
@@ -1,17 +1,56 @@
-function classNode(){
+console.log("Test")
+var l = "wow"
+console.log(l)
+var a = node()
+a.setName("a")
+console.log(a.name)
+var b = node()
+b.setName("b")
+a.addLink(b)
+console.log("Hello")
+
+function node(){
     name = null
+    nextNodes = []
+    return{
+        setName(newName){
+            this.name = newName
+        },
+        getName(){
+            return name
+        },
+        addLink(node){
+            nextNodes.push(node);
+            console.log(node.name + " has been pushed")
+        }
+    }
+}
+
+
+function classNode(){
+    classNode.prototype = Object.create(node.prototype)
     attributes = null
     methods = null
+    return{
+        setAttributes(newAttributes){
+            node.attributes = newAttributes
+        },
+        setMethods(newMethods){
+            node.methods = newMethods
+        }
+    }
 }
 
-function setName(node, newName){
-    node.name = newName
-}
-
-function setAttributes(node, newAttributes){
-    node.attributes = newAttributes
-}
-
-function setMethods(node, newMethods){
-    node.methods = newMethods
+function inheritanceNode(){
+    inheritanceNode.prototype = Object.create(node.prototype)
+    attributes = null
+    methods = null
+    return{
+        setAttributes(newAttributes){
+            node.attributes = newAttributes
+        },
+        setMethods(newMethods){
+            node.methods = newMethods
+        }
+    }
 }
