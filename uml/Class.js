79ebfa80f790f33091cc1e52bc80cc8e5baff3db
Jeffrey Wu <Jeffrey.S.Wu@sjsu.edu>
2019-04-20 14:52:17 -0700

    class node js file added.

---
+++ uml/Class.js
@@ -0,0 +1,17 @@
+function classNode(){
+    name = null
+    attributes = null
+    methods = null
+}
+
+function setName(node, newName){
+    node.name = newName
+}
+
+function setAttributes(node, newAttributes){
+    node.attributes = newAttributes
+}
+
+function setMethods(node, newMethods){
+    node.methods = newMethods
+}
