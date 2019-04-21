0d0c56baee966b0729075203ca43077a3c0340e7
Nhu Nguyen <nhutnguyen5691@gmail.com>
2019-04-20 22:15:36 -0700

    ...

--- graph/Graph.js
+++ graph/Graph.js
@@ -1,29 +1,30 @@
-'use strict'
+'use strict';
 
-function createGraphFramework(){
-    
-}
+function createGraphFramework() {
 
-const nodePrototype = {
-    draw: function(g2) {
-         
-    },
-    translate: function(dx, dy){
-         
-    },
-    contains: function(point){},
-    getConnectionPoint: function(point){},
-    getBounds: function(){},
-    clone: function(){}
-}
 
-const edgePrototype = {
-    draw: function(g2){},
-    contains: function(point){},
-    connect: function(start, end){},
-    getStart: function(){},
-    getEnd: function(){},
-    getConnectionPoints: function(){},
-    getBounds: function(g2){},
-    clone: function(){}
+    const nodePrototype = {
+        draw: function (g2) {
+
+        },
+        translate: function (dx, dy) {
+
+        },
+        contains: function (point) { },
+        getConnectionPoint: function (point) { },
+        getBounds: function () { },
+        clone: function () { }
+    }
+
+    const edgePrototype = {
+        draw: function (g2) { },
+        contains: function (point) { },
+        connect: function (start, end) { },
+        getStart: function () { },
+        getEnd: function () { },
+        getConnectionPoints: function () { },
+        getBounds: function (g2) { },
+        clone: function(){}   
+    } 
+        
 }
