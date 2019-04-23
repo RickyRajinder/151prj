881991928ee6d3803a3d03ccb0542b82833c838c
rickyrajinder <singh.sangh@gmail.com>
2019-04-22 20:02:09 -0700

    Toolbar upgrades

--- uml/Class.js
+++ uml/Class.js
@@ -7,23 +7,110 @@ var b = node()
 b.setName("b")
 a.addLink(b, "Aggregation")
 
-$(function () {
-    $('#classtoolbar').w2toolbar({
-        name: 'toolbar',
-        items: [
-            {type: 'button', id: 'select', icon: 'rectangle', caption: 'Select'},
-            {type: 'break' },
-            {type: 'button', id: 'class', caption: 'Class'},
-            {type: 'break' },
-            {type: 'button', id: 'interface', caption: 'Interface'},
-            {type: 'break' },
-            {type: 'button', id: 'package', caption: 'Package'},
-            {type: 'break' },
-            {type: 'button', id: 'note', caption: 'Note'},
-            {type: 'break' },
-        ]
-    });
-});
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+function drawToolBar() {
+    const bar = new Path2D()
+    ctx.lineWidth = 2
+    bar.rect(5, 5, 990, 50)
+    ctx.rect(5,5,990,50)
+    ctx.fillStyle = '#99e6ff'
+    ctx.fill(bar)
+    ctx.stroke()
+}
+
+function drawSelectButton(){
+    let select = new Path2D()
+    select.rect(10, 10, 50, 40)
+    ctx.stroke(select)
+    ctx.fillStyle = '#000000'
+    const box = ctx.fillRect(20, 20, 6, 6)
+    ctx.fillRect(40, 20, 6, 6)
+    ctx.fillRect(20, 35, 6, 6)
+    ctx.fillRect(40, 35, 6, 6)
+    canvas.addEventListener('mousemove', function(e) {
+        if (ctx.isPointInPath(select, e.clientX, e.clientY)){
+            ctx.font = "15px Arial"
+            ctx.fillText("Select", 15, 70)
+        }
+        else {
+            ctx.clearRect(0,0, canvas.width, canvas.height)
+            drawToolBar()
+            drawClassButton()
+            drawInterfaceButton()
+            let select = new Path2D()
+            select.rect(10, 10, 50, 40)
+            ctx.stroke(select)
+            ctx.fillStyle = '#000000'
+            const box = ctx.fillRect(20, 20, 6, 6)
+            ctx.fillRect(40, 20, 6, 6)
+            ctx.fillRect(20, 35, 6, 6)
+            ctx.fillRect(40, 35, 6, 6)
+        }
+    })
+}
+
+function drawClassButton(){
+    let classButton = new Path2D()
+    classButton.rect(63, 10, 50, 40)
+    ctx.fillStyle = "white"
+    ctx.fillRect(73,20, 30, 6)
+    ctx.fillRect(73,26, 30, 6)
+    ctx.fillRect(73,32, 30, 6)
+    ctx.rect(73,20,30,6)
+    ctx.rect(73,26,30,6)
+    ctx.rect(73,32,30,6)
+    ctx.stroke(classButton)
+    ctx.stroke()
+    canvas.addEventListener('mousemove', function(e) {
+        if (ctx.isPointInPath(classButton, e.clientX, e.clientY)){
+            ctx.font = "15px Arial"
+            ctx.fillText("Class", 68, 70)
+        }
+        else {
+        }
+    })
+}
+
+function drawInterfaceButton(){
+    let interfaceButton = new Path2D()
+    interfaceButton.rect(116, 10, 50, 40)
+    ctx.stroke(interfaceButton)
+    ctx.fillStyle = "white"
+    ctx.fillRect(126,20, 30, 6)
+    ctx.fillRect(126,26, 30, 6)
+    ctx.fillRect(126,32, 30, 6)
+    ctx.rect(126,20,30,6)
+    ctx.rect(126,26,30,6)
+    ctx.rect(126,32,30,6)
+    ctx.stroke()
+    ctx.font = "8px Arial"
+    ctx.fillStyle= "#000"
+    ctx.fillText("Interface", 125, 26)
+    canvas.addEventListener('mousemove', function(e) {
+        if (ctx.isPointInPath(interfaceButton, e.clientX, e.clientY)){
+            ctx.font = "lighter 15px Arial"
+            ctx.fillText("Interface", 118, 70)
+        }
+        else {
+        }
+    })
+}
+
+function drawPackageButton(){
+    let packageButton = new Path2D()
+    packageButton.rect(169, 10, 50, 40)
+    ctx.stroke(packageButton)
+    ctx.fillStyle = "white"
+
+}
+
+drawToolBar()
+drawSelectButton()
+drawClassButton()
+drawInterfaceButton()
+
 function node(){
     const name = null
     return{
