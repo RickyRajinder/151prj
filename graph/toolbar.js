888784f5525b7a8e963c5d5d453d5573b62d92b7
NhuNguyen <nhutnguyen5691@gmail.com>
2019-04-29 16:48:22 -0700

    fixing toolbar

--- graph/toolbar.js
+++ graph/toolbar.js
@@ -1,38 +1,41 @@
 'use strict'
 class Toolbar {
-    constructor(buttons) {
-        this.buttons = []
-        this.tools = []
-        
-        
-    }
+	constructor(buttons) {
+		this.buttons = []
+		this.tools = []
 
-    add(MenuButton) {
-        buttons.push(MenuButton)
-    }
 
-    getSelectedTool() {
-        for (let i = 0; i < buttons.length; i++)
-            if (buttons[i].isSelected)
-                return buttons[i];
-    }
+	}
 
-    draw() {
-        const bar = new Path2D()
-        ctx.lineWidth = 2
-        bar.rect(5, 5, 990, 50)
-        ctx.rect(5, 5, 990, 50)
-        ctx.fillStyle = '#99e6ff'
-        ctx.fill(bar)
-        ctx.stroke()
-    }
+	add(MenuButton) {
+		buttons.push(MenuButton)
+	}
 
-    drawButtons(){
-        this.buttons.forEach(function(){drawButtons(button)
-        });
+	getSelectedTool() {
+		for (let i = 0; i < buttons.length; i++)
+			if (buttons[i].isSelected)
+				return buttons[i];
+	}
 
-    
+	draw() {
+		const bar = new Path2D()
+		ctx.lineWidth = 2
+		bar.rect(5, 5, 990, 50)
+		ctx.rect(5, 5, 990, 50)
+		ctx.fillStyle = '#99e6ff'
+		ctx.fill(bar)
+		ctx.stroke()
+	}
 
+	drawButtons() {
+		this.buttons.forEach(function() {
+			
+		});
+
+		
 
 
 }
+    
+}
+	
