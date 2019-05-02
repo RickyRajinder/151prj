972c420b6e33827811081db7bf687ce6be02a624
Nhu Nguyen <nhutnguyen5691@gmail.com>
2019-05-01 23:21:07 -0700

    partial impl toolbar + menubutton. Add globals. Move simple editor to separate project

--- graph/menuButton.js
+++ graph/menuButton.js
@@ -1,16 +1,88 @@
 'use strict'
-class MenuButton {
-    constructor(action) {//action: a function passed in by user for the listener to execute
-        //type: of button (node, edge, ...)
-        this.eventListener = document.addEventListener('click', action);
-        isSelected = false;
+
+/**
+ * Rectangular menu button
+ */
+class MenuButton {    
+
+    /**
+     * Construct rectangular menu button
+     * @param {Function} action a function passed in by user for the listener to execute
+     * @param {Integer} x horizontal position of button
+     * @param {Integer} y vertical position of button
+     * @param {Integer} width width of button
+     * @param {Integer} height height of button
+     * @param {Drawable} drawable drawable object
+     */
+    constructor(x, y, width, height, 
+        drawable = null, background = 'gray') {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.drawable = drawable;
+        this.background = background;
+        this.isSelected = false;
     }
 
+    /**
+     * Check if this button contain given point
+     * @param {number} x  
+     * @param {number} y 
+     */
+    contains(x, y) {
+        leftBound = this.x
+        rightBoud = this.x + this.width
+        upperBound = this.y
+        lowerBound = this.y + this.height
+        return (x > leftBound && x < rightBound) &&
+               (y > upperBound && y < lowerBound)
+    }
+
+    /**
+     * Determine if this button is selected
+     */
     isSelected(){
         return isSelected;
     }
 
-    setSelected(){
-        isSelected = true;
+    /**
+     * Select this button
+     * @param {Boolean} bool true or false
+     */
+    setSelect(bool){
+        isSelected = bool;
+    }
+
+    /**
+     * To be extended
+     */
+    draw(g2) {
+        // Draw button
+        savedStyle = g2.fillStyle
+        g2.fillStyle = this.background
+        g2.fillRect(this.x, this.y, this.width, this.height)
+        g2.fillStyle = savedStyle
+        this.drawable.draw()
+
+        // Draw button's icon
+        hPadding = this.width * 1 / 6;  // Hardcoded ratio. 
+        vPadding = this.height * 1 / 6; // Increase for smaller icon
+        iconX = this.x + padding;
+        iconY = this.y + padding;
+        iconW = this.width - padding;
+        iconH = this.height - padding;
+        bound = this.drawable.getBounds()
+        ratioX = iconW / bound.width
+        ratioY = iconH / bound.height
+        g2.scale(ratioX, ratioY)
+        deltaX = bound.x - iconX
+        deltaY = bound.y - iconY
+        g2.translate(deltaX, deltaY)
+        this.drawable.draw()
+
+        //Restore
+        g2.translate(-deltaX, deltaY)
+        g2.scale(1/ratioX, 1/ratioY)
     }
 }
--- graph/toolbar.js
+++ graph/toolbar.js
@@ -1,37 +1,63 @@
 'use strict'
+
+/**
+ * Toolbar which contains multiple tools/buttons
+ */
 class Toolbar {
-	constructor(buttons) {
-		this.buttons = []
-		this.tools = []
+    /**
+     * Construct new toolbar
+     * @param {*} canvas html canvas element
+     * @param {*} x top-left-x position 
+     * @param {*} y top-left-y position
+     * @param {*} width width of toolbar
+     * @param {*} height height of toolbar
+     */
+	constructor(canvas, x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.start = x
+        this.buttons = []
+        canvas.addEventListener("click", handle, false)
+    }
+    
+    /**
+     * Event handler for this toolbar.
+     * Invoke the action of the button being clicked
+     * @param {Event} event 
+     */
+    handle(event) {
+        for (button in this.buttons) {
+            if (button.contains(event.clientX, event.clientY)) {
+                button.action()
+            }
+        }
+    }
 
-
+    /**
+     * Add new button to this toolbar
+     * @param {function} action action for button being added
+     * @param {drawble} drawable a drawable object for drawing button icon 
+     */
+	addButton(width, height, action, drawable) {
+        paddingX = 3 // pixels
+        paddingY = 3 // pixels
+        button = new MenuButton(start + paddingX, y + paddingY, 
+                        width, height + paddingY)
+        
 	}
 
-	add(MenuButton) {
-		buttons.push(MenuButton)
-	}
+	draw(g2) {
 
-	getSelectedTool() {
-		for (let i = 0; i < buttons.length; i++)
-			if (buttons[i].isSelected)
-				return buttons[i];
-	}
-
-	draw() {
-		const bar = new Path2D()
-		ctx.lineWidth = 2
-		bar.rect(5, 5, 990, 50)
-		ctx.rect(5, 5, 990, 50)
-		ctx.fillStyle = '#99e6ff'
-		ctx.fill(bar)
-		ctx.stroke()
+		g2.fillRect(this.x, this.y, this.width, this.height)
 	}
 
 	drawButtons() {
 		this.buttons.forEach(function() {
 			
-		});
-}
+	    });
+    }
     
 }
 	
