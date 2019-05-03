d084c33b3924bc0fdd1a39dac4664a2a76e3f7ce
rickyrajinder <singh.sangh@gmail.com>
2019-05-02 20:50:57 -0700

    All nodes noew editable

--- uml/nodes.js
+++ uml/nodes.js
@@ -132,6 +132,50 @@ Node.prototype.interfaceNode = function (x,y) {
     }
 }
 
+Node.prototype.interfaceNodeUpdated = function (x,y, strarr) {
+    this.x = x
+    this.y = y
+    this.strarr = strarr
+    return {
+        getBounds: () => {
+            return {
+                x: x,
+                y: y,
+                width: 100,
+                height: 100
+            }
+        },
+        contains: p => {
+            return (x + 100 / 2 - p.x) ** 2 + (y + 100 / 2 - p.y) ** 2 <= 100 ** 2 / 4
+        },
+        translate: (dx, dy) => {
+            x += dx
+            y += dy
+        },
+        draw: () => {
+            const canvas = document.getElementById('canvas')
+            const ctx = canvas.getContext('2d')
+            const data = "<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'>" +
+                "<foreignObject width='100%' height='100%'>" +
+                "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:10px'>" +
+                "<table border='1'><tr><td>" + this.strarr[0] + "</td></tr><tr><td>" + this.strarr[1] + "</td></tr><tr><td>" + this.strarr[2] + "</td></tr></table>" +
+                "</div>" +
+                "</foreignObject>" +
+                "</svg>";
+
+            const DOMURL = self.URL || self.webkitURL || self;
+            const img = new Image();
+            const svg = new Blob([data], {type: "image/svg+xml;charset=utf-8"});
+            const url = DOMURL.createObjectURL(svg);
+            img.onload = function () {
+                ctx.drawImage(img, x, y);
+                DOMURL.revokeObjectURL(url);
+            };
+            img.src = url;
+        }
+    }
+}
+
 Node.prototype.packageNode = function (x,y) {
     this.x = x
     this.y = y
@@ -176,6 +220,51 @@ Node.prototype.packageNode = function (x,y) {
     }
 }
 
+Node.prototype.packageNodeUpdated = function (x,y, strarr) {
+    this.x = x
+    this.y = y
+    this.strarr = strarr
+    return {
+        getBounds: () => {
+            return {
+                x: x,
+                y: y,
+                width: 100,
+                height: 100
+            }
+        },
+        contains: p => {
+            return (x + 100 / 2 - p.x) ** 2 + (y + 100 / 2 - p.y) ** 2 <= 100 ** 2 / 4
+        },
+        translate: (dx, dy) => {
+            x += dx
+            y += dy
+        },
+        draw: () => {
+            const canvas = document.getElementById('canvas')
+            const ctx = canvas.getContext('2d')
+            const data = "<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'>" +
+                "<foreignObject width='100%' height='100%'>" +
+                "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:10px'>" +
+                "<table border='1'><tr><td>______</td></tr></table>" +
+                "<table border='1'><tr><td>" + this.strarr[0] +"</td></tr></table>" +
+                "</div>" +
+                "</foreignObject>" +
+                "</svg>";
+
+            const DOMURL = self.URL || self.webkitURL || self;
+            const img = new Image();
+            const svg = new Blob([data], {type: "image/svg+xml;charset=utf-8"});
+            const url = DOMURL.createObjectURL(svg);
+            img.onload = function () {
+                ctx.drawImage(img, x, y);
+                DOMURL.revokeObjectURL(url);
+            };
+            img.src = url;
+        }
+    }
+}
+
 Node.prototype.noteNode = function (x,y) {
     this.x = x
     this.y = y
@@ -202,6 +291,50 @@ Node.prototype.noteNode = function (x,y) {
                 "<foreignObject width='100%' height='100%'>" +
                 "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:15px'>" +
                 "<table border='0' style='background-color:#eae364'><tr><td>Note: </td></tr></table>" +
+                "</div>" +
+                "</foreignObject>" +
+                "</svg>"
+
+            const DOMURL = self.URL || self.webkitURL || self
+            const img = new Image()
+            const svg = new Blob([data], {type: "image/svg+xml;charset=utf-8"})
+            const url = DOMURL.createObjectURL(svg)
+            img.onload = function () {
+                ctx.drawImage(img, x, y)
+                DOMURL.revokeObjectURL(url)
+            }
+            img.src = url
+        }
+    }
+}
+
+Node.prototype.noteNodeUpdated = function (x,y, strarr) {
+    this.x = x
+    this.y = y
+    this.strarr = strarr
+    return {
+        getBounds: () => {
+            return {
+                x: x,
+                y: y,
+                width: 100,
+                height: 100
+            }
+        },
+        contains: p => {
+            return (x + 100 / 2 - p.x) ** 2 + (y + 100 / 2 - p.y) ** 2 <= 100 ** 2 / 4
+        },
+        translate: (dx, dy) => {
+            x += dx
+            y += dy
+        },
+        draw: () => {
+            const canvas = document.getElementById('canvas')
+            const ctx = canvas.getContext('2d')
+            const data = "<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'>" +
+                "<foreignObject width='100%' height='100%'>" +
+                "<div xmlns='http://www.w3.org/1999/xhtml' style='font-size:15px'>" +
+                "<table border='0' style='background-color:#eae364'><tr><td>" + this.strarr[0] +  "</td></tr></table>" +
                 "</div>" +
                 "</foreignObject>" +
                 "</svg>"
